c1b392c812921b467497fdc450a94020cfe9afbd
刘俊宏 <731588013@qq.com>
2016-04-04 19:47:11 +0800

    Initial Commit

---
+++ ayi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ayi
+//
+//  Created by 刘俊宏 on 16/4/4.
+//  Copyright © 2016年 刘俊宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ayi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ayi
+//
+//  Created by 刘俊宏 on 16/4/4.
+//  Copyright © 2016年 刘俊宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
