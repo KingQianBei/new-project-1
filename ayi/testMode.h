b88e1bf68cefe9d0629a1bc340ec609ab886a8d9
刘俊宏 <731588013@qq.com>
2016-04-04 19:49:15 +0800

    new test

---
+++ ayi/testMode.h
@@ -0,0 +1,13 @@
+//
+//  testMode.h
+//  ayi
+//
+//  Created by 刘俊宏 on 16/4/4.
+//  Copyright © 2016年 刘俊宏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testMode : NSObject
+
+@end
---
+++ ayi/testMode.m
@@ -0,0 +1,13 @@
+//
+//  testMode.m
+//  ayi
+//
+//  Created by 刘俊宏 on 16/4/4.
+//  Copyright © 2016年 刘俊宏. All rights reserved.
+//
+
+#import "testMode.h"
+
+@implementation testMode
+
+@end
